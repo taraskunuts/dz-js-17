6a0ae6a9451bbec5907435ace28cfe278f6f00a2
taraskunuts <117195745+taraskunuts@users.noreply.github.com>
2025-02-03 17:49:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const button = document.getElementById("myButton")
+const inputField = document.getElementById("mainInput")
+button.textContent = inputField.value
+//
+//
+//
+//
+const image = document.getElementById("mySani")
+image.src = "https://content1.rozetka.com.ua/goods/images/big/416152711.jpg"
+//
+//
+//
+//
+const link = document.getElementById("myRozetka");
+link.href = "https://www.roblox.com/home"
+//
+//
+//
+//
+const linkImage = document.getElementById("animAllLink")
+linkImage.alt = "AnimAll"
+//
+//
+//
+const firstItem = document.getElementById("firstElement")
+firstItem.textContent = "перший елемент!!11!1!!!"
